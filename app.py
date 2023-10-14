5b124d9c4ca814c776d09f91ed056590fe4e6be4
Эльнур Гулиев <marin@air-elnur.server.local>
2023-10-14 21:10:02 +0500

    fix cors

--- app.py
+++ app.py
@@ -2,9 +2,22 @@ from fastapi import FastAPI
 from routers.azreil import AzreilRouters
 from routers.ismail import IsmailRouters
 from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
+
 load_dotenv()
 
+origins = [
+    "*"
+]
+
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 app.include_router(AzreilRouters)
