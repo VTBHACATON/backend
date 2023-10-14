3c6d9c35a0005244bd4db694b290934b4f7fd3f5
Эльнур Гулиев <marin@MacBook-Air-Elnur.local>
2023-10-14 23:42:22 +0500

    fixed router_traffic

--- backend/routers/ismail/router_traffic.py
+++ backend/routers/ismail/router_traffic.py
@@ -14,7 +14,7 @@ async def traffic_routing(id: str | None = None, town: str | None = None):
                 print(date)
                 return date.to_dict()
 
-        raise HTTPException(status_code=404, detail="Item not found")
+        raise HTTPException(status_code=404, detail="Id not found")
     elif town:
         towns = []
         for date in traffics:
@@ -23,3 +23,39 @@ async def traffic_routing(id: str | None = None, town: str | None = None):
         return towns
 
     return [traffic.to_dict() for traffic in traffics]
+
+
+async def calculate_percent(traffic) -> float:
+    traffic_data = traffic.to_dict()
+    now = traffic_data["traffic"]
+    max = traffic_data["max"]
+    return round(now * 100 / max, 2)
+
+
+@testRouter.get("/traffic/rating", tags=["rating"])
+async def traffic_rating(id: str | None = None, town: str | None = None):
+    traffics = database.get_points_traffic()
+    answer = {}
+
+    for traffic in traffics:
+        percent = await calculate_percent(traffic)
+        answer[traffic.id] = percent
+
+    sorted_answer = dict(sorted(answer.items(), key=lambda item: item[1]))
+
+    if id:
+        return {id: sorted_answer.get(id)}
+
+    if town:
+        points = [
+            {"id": traffic.id, "town": traffic.town, "percent": percent}
+            for traffic, percent in zip(traffics, answer.values())
+            if traffic.town == town
+        ]
+
+        if points:
+            return sorted(points, key=lambda item: item["percent"])
+
+        raise HTTPException(status_code=404, detail="Town not found")
+
+    return sorted_answer
--- test.py
+++ test.py
@@ -1,8 +1,23 @@
 
-from backend.services.azreil.office import Office
+# from backend.services.azreil.office import Office
+# from backend.utils import database
+# from random import randint
+# towns = Office.get_all()
+# townss = {'Подольск,', 'Мытищи,', 'Химки,', 'Долгопрудный,', 'Солнечногорск,',
+#           'Moскoвский,', 'Пушкино,', 'Дедовск,', 'Лобня,', 'Железнодорожный,',
+#           'Красногорск,', 'Люберцы,', 'Москва', 'Дзержинский,', 'Фрязино,',
+#           'Раменское,', 'Черноголовка,', 'Москва,', 'Балашиха,', 'Звенигород,',
+#           'Реутов,', 'Видное,', 'Жуковский,', 'Одинцово,', 'Истра,', 'Щелково,',
+#           'Королев,', 'Ивантеевка,', 'Домодедово,'}
 
-towns = Office.get_all()
-
-print(towns[1]["id"])
-for town in towns:
-    print(town["address"])
+# print(towns[1]["id"])
+# for town in towns:
+#     adr: list = town["address"].split()
+#     try:
+#         num = adr.index("г.")
+#         gorod = adr[num+1][0:-1]
+#         database.register_point_traffic(
+#             town["id"], gorod, randint(0, 400), randint(200, 600))
+#     except:
+#         pass
+# print(townss)
